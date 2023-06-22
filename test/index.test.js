c8cf41478bdee0aacfdf565df064b9eb02f68b8d
Rudi Wahyudi <diruuu@yahoo.com>
2023-06-22 14:30:04 +0700

    Added setTimestamps option

--- test/index.test.js
+++ test/index.test.js
@@ -85,6 +85,35 @@ describe('mongocruise - plugin', () => {
     expect(result).to.be.an.object()
   })
 
+  it('adds createdAt and updatedAt on the insertOne operation', async () => {
+    const handlerOptions = {
+      collection: 'users',
+      operation: 'insertOne'
+    }
+
+    plugin.register(serverMock)
+
+    const handler = serverMock.decorate.args[0][2]({}, handlerOptions)
+    const result = await handler({ payload: { name: 'John' } }, serverMock)
+    expect(Object.keys(serverMock.mongo.db.insertOne.getCall(0).args[0])).to.equal(['name', 'createdAt', 'updatedAt'])
+    expect(result).to.be.an.object()
+  })
+
+  it('omits createdAt and updatedAt on the insertOne operation if setTimestamps is set to false', async () => {
+    const handlerOptions = {
+      collection: 'users',
+      operation: 'insertOne',
+      setTimestamps: false
+    }
+
+    plugin.register(serverMock)
+
+    const handler = serverMock.decorate.args[0][2]({}, handlerOptions)
+    const result = await handler({ payload: { name: 'John' } }, serverMock)
+    expect(Object.keys(serverMock.mongo.db.insertOne.getCall(0).args[0])).to.equal(['name'])
+    expect(result).to.be.an.object()
+  })
+
   it('correctly calls the updateOne operation', async () => {
     const handlerOptions = {
       collection: 'users',
@@ -98,6 +127,41 @@ describe('mongocruise - plugin', () => {
     const result = await handler({ params: { id: '123' }, payload: { name: 'John' } }, serverMock)
 
     expect(serverMock.mongo.db.collection.calledWith('users')).to.be.true()
+    expect(serverMock.mongo.db.updateOne.calledOnce).to.be.true()
+    expect(result).to.be.an.object()
+  })
+
+  it('adds updatedAt on the updateOne operation', async () => {
+    const handlerOptions = {
+      collection: 'users',
+      operation: 'updateOne',
+      queryParam: 'id'
+    }
+
+    plugin.register(serverMock)
+
+    const handler = serverMock.decorate.args[0][2]({ params: { id: '123' } }, handlerOptions)
+    const result = await handler({ params: { id: '123' }, payload: { name: 'John' } }, serverMock)
+
+    expect(Object.keys(serverMock.mongo.db.updateOne.getCall(0).args[1].$set)).to.equal(['name', 'updatedAt'])
+    expect(serverMock.mongo.db.updateOne.calledOnce).to.be.true()
+    expect(result).to.be.an.object()
+  })
+
+  it('omits updatedAt on the updateOne operation if setTimestamps is set to false', async () => {
+    const handlerOptions = {
+      collection: 'users',
+      operation: 'updateOne',
+      queryParam: 'id',
+      setTimestamps: false
+    }
+
+    plugin.register(serverMock)
+
+    const handler = serverMock.decorate.args[0][2]({ params: { id: '123' } }, handlerOptions)
+    const result = await handler({ params: { id: '123' }, payload: { name: 'John' } }, serverMock)
+
+    expect(Object.keys(serverMock.mongo.db.updateOne.getCall(0).args[1].$set)).to.equal(['name'])
     expect(serverMock.mongo.db.updateOne.calledOnce).to.be.true()
     expect(result).to.be.an.object()
   })
