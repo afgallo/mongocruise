039f5b60f759bdf2a0b3018492bf59fb4b996bb4
Marco Bomfim <marcoabomfim3@gmail.com>
2023-10-12 11:12:33 +1100

    fix: test cases to check for meaningful information

--- test/index.test.js
+++ test/index.test.js
@@ -2,33 +2,49 @@ const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 const Sinon = require('sinon')
 const { plugin } = require('../lib')
+const { MongoClient, ObjectId } = require('mongodb')
+const { MongoMemoryServer } = require('mongodb-memory-server')
 
-const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
+const { describe, it, beforeEach, afterEach, before, after } = (exports.lab = Lab.script())
 const { expect } = Code
 
+const mockedUsers = [
+  { _id: new ObjectId(123), name: 'John', email: 'john@example.com' },
+  { _id: new ObjectId(456), name: 'Jones', email: 'jones@example.com' }
+]
+
 describe('mongocruise - plugin', () => {
+  let db
+  let mongoClient
+  let mongoServer
   let serverMock
 
-  beforeEach(() => {
+  before(async () => {
+    mongoServer = await MongoMemoryServer.create()
+    mongoClient = await MongoClient.connect(mongoServer.getUri(), {})
+    db = mongoClient.db(mongoServer.instanceInfo.dbName)
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').insertMany([...mockedUsers])
+
     serverMock = {
       mongo: {
-        db: {
-          collection: Sinon.stub().returnsThis(),
-          find: Sinon.stub().returnsThis(),
-          findOne: Sinon.stub().resolves({}),
-          insertOne: Sinon.stub().resolves({}),
-          updateOne: Sinon.stub().resolves({}),
-          deleteOne: Sinon.stub().resolves({}),
-          toArray: Sinon.stub().resolves([])
-        },
-        ObjectId: Sinon.stub()
+        db,
+        ObjectId
       },
       decorate: Sinon.stub()
     }
   })
 
-  afterEach(() => {
+  afterEach(async () => {
+    await db.collection('users').drop()
     Sinon.restore()
+  })
+
+  after(async () => {
+    if (mongoClient) await mongoClient.close()
+    if (mongoServer) await mongoServer.stop()
   })
 
   it('registers correctly', () => {
@@ -47,21 +63,26 @@ describe('mongocruise - plugin', () => {
     const handler = serverMock.decorate.args[0][2]({}, handlerOptions)
     const result = await handler({ query: {} }, serverMock)
 
-    expect(serverMock.mongo.db.collection.calledWith('users')).to.be.true()
-    expect(serverMock.mongo.db.find.calledOnce).to.be.true()
     expect(result).to.be.an.array()
+    expect(result.length).to.equal(2)
+    expect(result[0].name).to.equal('John')
   })
 
   it('correctly calls the findOne operation', async () => {
     const handlerOptions = {
       collection: 'users',
       operation: 'findOne',
-      queryParam: 'id'
+      queryParam: '_id'
     }
 
     plugin.register(serverMock)
 
-    const handler = serverMock.decorate.args[0][2]({ params: { id: '123' } }, handlerOptions)
+    const handler = serverMock.decorate.args[0][2]({ params: { _id: mockedUsers[0]._id } }, handlerOptions)
+    const result = await handler({ params: { _id: mockedUsers[0]._id } }, serverMock)
+
+    expect(result).to.be.an.object()
+    expect(result.name).to.equal('John')
+  })
 
   it('correctly calls the findOne operation with a projection', async () => {
     const handlerOptions = {
@@ -91,11 +112,10 @@ describe('mongocruise - plugin', () => {
     plugin.register(serverMock)
 
     const handler = serverMock.decorate.args[0][2]({}, handlerOptions)
-    const result = await handler({ payload: { name: 'John' } }, serverMock)
+    const result = await handler({ payload: { name: 'Anderson' } }, serverMock)
 
-    expect(serverMock.mongo.db.collection.calledWith('users')).to.be.true()
-    expect(serverMock.mongo.db.insertOne.calledOnce).to.be.true()
     expect(result).to.be.an.object()
+    expect(result.name).to.equal('Anderson')
   })
 
   it('adds createdAt and updatedAt on the insertOne operation', async () => {
@@ -107,9 +127,10 @@ describe('mongocruise - plugin', () => {
     plugin.register(serverMock)
 
     const handler = serverMock.decorate.args[0][2]({}, handlerOptions)
-    const result = await handler({ payload: { name: 'John' } }, serverMock)
-    expect(Object.keys(serverMock.mongo.db.insertOne.getCall(0).args[0])).to.equal(['name', 'createdAt', 'updatedAt'])
+    const result = await handler({ payload: { name: 'Leila' } }, serverMock)
+
     expect(result).to.be.an.object()
+    expect(Object.keys(result)).to.equal(['_id', 'name', 'createdAt', 'updatedAt'])
   })
 
   it('omits createdAt and updatedAt on the insertOne operation if setTimestamps is set to false', async () => {
@@ -123,77 +144,83 @@ describe('mongocruise - plugin', () => {
 
     const handler = serverMock.decorate.args[0][2]({}, handlerOptions)
     const result = await handler({ payload: { name: 'John' } }, serverMock)
-    expect(Object.keys(serverMock.mongo.db.insertOne.getCall(0).args[0])).to.equal(['name'])
+
     expect(result).to.be.an.object()
+    expect(Object.keys(result)).to.equal(['_id', 'name'])
   })
 
   it('correctly calls the updateOne operation', async () => {
     const handlerOptions = {
       collection: 'users',
       operation: 'updateOne',
-      queryParam: 'id'
+      queryParam: '_id'
     }
 
     plugin.register(serverMock)
 
-    const handler = serverMock.decorate.args[0][2]({ params: { id: '123' } }, handlerOptions)
-    const result = await handler({ params: { id: '123' }, payload: { name: 'John' } }, serverMock)
+    const handler = serverMock.decorate.args[0][2]({ params: { _id: mockedUsers[0]._id } }, handlerOptions)
+    const result = await handler({ params: { _id: mockedUsers[0]._id }, payload: { name: 'Updated John' } }, serverMock)
 
-    expect(serverMock.mongo.db.collection.calledWith('users')).to.be.true()
-    expect(serverMock.mongo.db.updateOne.calledOnce).to.be.true()
-    expect(result).to.be.an.object()
+    expect(result.acknowledged).to.be.true()
+    expect(result.modifiedCount).to.be.greaterThan(0)
   })
 
   it('adds updatedAt on the updateOne operation', async () => {
-    const handlerOptions = {
+    const updateHandlerOptions = {
       collection: 'users',
       operation: 'updateOne',
-      queryParam: 'id'
+      queryParam: '_id'
     }
 
     plugin.register(serverMock)
 
-    const handler = serverMock.decorate.args[0][2]({ params: { id: '123' } }, handlerOptions)
-    const result = await handler({ params: { id: '123' }, payload: { name: 'John' } }, serverMock)
+    const handler = serverMock.decorate.args[0][2]({ params: { _id: mockedUsers[0]._id } }, updateHandlerOptions)
+    const result = await handler({ params: { _id: mockedUsers[0]._id }, payload: { name: 'New Name' } }, serverMock)
 
-    expect(Object.keys(serverMock.mongo.db.updateOne.getCall(0).args[1].$set)).to.equal(['name', 'updatedAt'])
-    expect(serverMock.mongo.db.updateOne.calledOnce).to.be.true()
-    expect(result).to.be.an.object()
+    expect(result.acknowledged).to.be.true()
+    expect(result.modifiedCount).to.be.greaterThan(0)
+
+    const updatedDocument = await db.collection('users').findOne({ name: 'New Name' })
+    expect(updatedDocument).to.be.an.object()
+    expect(updatedDocument.updatedAt).to.exist()
   })
 
   it('omits updatedAt on the updateOne operation if setTimestamps is set to false', async () => {
     const handlerOptions = {
       collection: 'users',
       operation: 'updateOne',
-      queryParam: 'id',
+      queryParam: '_id',
       setTimestamps: false
     }
 
     plugin.register(serverMock)
 
-    const handler = serverMock.decorate.args[0][2]({ params: { id: '123' } }, handlerOptions)
-    const result = await handler({ params: { id: '123' }, payload: { name: 'John' } }, serverMock)
+    const handler = serverMock.decorate.args[0][2]({ params: { _id: mockedUsers[0]._id } }, handlerOptions)
+    const result = await handler({ params: { _id: mockedUsers[0]._id }, payload: { name: 'Omitted' } }, serverMock)
 
-    expect(Object.keys(serverMock.mongo.db.updateOne.getCall(0).args[1].$set)).to.equal(['name'])
-    expect(serverMock.mongo.db.updateOne.calledOnce).to.be.true()
-    expect(result).to.be.an.object()
+    expect(result.acknowledged).to.be.true()
+    expect(result.modifiedCount).to.be.greaterThan(0)
+
+    const updatedDocument = await db.collection('users').findOne({ _id: mockedUsers[0]._id })
+
+    expect(updatedDocument).to.be.an.object()
+    expect(updatedDocument.updatedAt).to.not.exist()
   })
 
   it('correctly calls the deleteOne operation', async () => {
     const handlerOptions = {
       collection: 'users',
       operation: 'deleteOne',
-      queryParam: 'id'
+      queryParam: '_id'
     }
 
     plugin.register(serverMock)
 
-    const handler = serverMock.decorate.args[0][2]({ params: { id: '123' } }, handlerOptions)
-    const result = await handler({ params: { id: '123' } }, serverMock)
+    const handler = serverMock.decorate.args[0][2]({ params: { _id: mockedUsers[1]._id } }, handlerOptions)
+    const result = await handler({ params: { _id: mockedUsers[1]._id } }, serverMock)
 
-    expect(serverMock.mongo.db.collection.calledWith('users')).to.be.true()
-    expect(serverMock.mongo.db.deleteOne.calledOnce).to.be.true()
-    expect(result).to.be.an.object()
+    expect(result.acknowledged).to.be.true()
+    expect(result.deletedCount).to.be.greaterThan(0)
   })
 
   it('throws an error for invalid query', async () => {
